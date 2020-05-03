76439a64b7edf074fcdba4831af4370d2e8eb743
Santhosh Moorthy (WLT IN) <santhosh.moorthy@wlt.com>
2020-05-03 21:43:16 +0530

    Login & Registration validation wip

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -4,11 +4,12 @@ import { PagesComponent } from './pages.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { FormsModule } from '@angular/forms';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 
 @NgModule({
-  declarations: [PagesComponent, LoginComponent, RegistrationComponent],
+  declarations: [PagesComponent, LoginComponent, RegistrationComponent, ForgotPasswordComponent],
   imports: [
     CommonModule,
     FormsModule
