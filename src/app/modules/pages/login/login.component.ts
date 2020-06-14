0d98bde5d63e0ea0affd3fb5edd01dc001a4d4c7
Santhosh Moorthy (WLT IN) <santhosh.moorthy@wlt.com>
2020-06-14 12:32:58 +0530

    snackbar including

--- src/app/modules/pages/login/login.component.ts
+++ src/app/modules/pages/login/login.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from 'src/app/common/services/auth.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CommonService } from 'src/app/common/services/common.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NotificationComponent } from 'src/app/fragments/core/notification/notification.component';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +19,13 @@ export class LoginComponent implements OnInit {
   public error: string;
   @Input() clear: any;
 
-  constructor(private auth: AuthService, private router: Router, private ref: ChangeDetectorRef, private commonService:CommonService) { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private ref: ChangeDetectorRef,
+    private commonService:CommonService,
+    private _snackBar:MatSnackBar
+  ) { }
   userLoginForm = new FormGroup({
     loginUsername : new FormControl('', [
       Validators.required,
@@ -41,8 +49,20 @@ export class LoginComponent implements OnInit {
           this.commonService.sendSpinnerStatus(false);
           if (err.status == 401) {
             this.error = 'This mail id not registered.';
+            this._snackBar.openFromComponent(NotificationComponent, {
+              duration:5000,
+              data:'This mail id not registered.',
+              panelClass:"error",
+              verticalPosition:"top"
+            })
           } else {
-            this.error = err.error.message;
+            this._snackBar.openFromComponent(NotificationComponent, {
+              duration:5000,
+              data:err.error.message,
+              panelClass:"error",
+              verticalPosition:"top"
+            })
+            //this.error = err.error.message;
           }
         }
       );
